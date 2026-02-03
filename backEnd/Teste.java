e16e655ddf724f564df20f462371180dc85fd580
Davi <davisendin@gmail.com>
2026-02-02 21:52:44 -0300

    juntou

--- backEnd/Teste.java
+++ backEnd/Teste.java
@@ -1,9 +1,13 @@
 package backEnd;
 
+import frontEnd.*;
+import java.awt.EventQueue;
+
+
 public class Teste {
 
 	public static void main(String[] args) {
-		Motorista motorista = new Motorista("13289273695", "Motoristo da Silva", "Rua dos bobos numero 0", "14062006");
+		Motorista motorista = new Motorista("13289273695", "Motoristo da Silva","carlos", "Rua dos bobos numero 0", "14062006","1234");
 		if (motorista.cpfValido(motorista.getCpfString())) {
 			System.out.println("Sim");
 		} else {
@@ -15,6 +19,23 @@ public class Teste {
 		motorista.atualizaMedia((float)5.0);
 		motorista.atualizaMedia((float)20000000000000000.0);
 		System.out.println("Nova media: " + motorista.getMedia());
+		
+		
+				// Interface
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Uberland frame = new Uberland();
+					frame.navegarPaginas(new BemVindo(frame),"bemVindo");
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
+	
+		
+	
 
 }
